963a0f104cca77d1ff7217c6e1d9cb0592840df1
Elon-Rama <ramadevi@elonnativesystem.com>
2024-11-14 11:36:40 +0530

    create-socket

--- Controller/FireQuestion/fireController.js
+++ Controller/FireQuestion/fireController.js
@@ -2,7 +2,7 @@ const FireService = require("../../Service/FireQuestion/fireService");
 const { validationResult } = require("express-validator");
 
 exports.create = async (req, res) => {
-  //#swagger.tags = ['Questionpage-FIRE']
+  //#swagger.tags = ['Questionpage']
   try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -21,7 +21,7 @@ exports.create = async (req, res) => {
 };
 
 exports.calculate = async (req, res) => {
-  //#swagger.tags = ['Questionpage-FIRE']
+  //#swagger.tags = ['Questionpage']
   try {
     const { fireId } = req.params;
     const response = await FireService.calculateRetirement(fireId);
